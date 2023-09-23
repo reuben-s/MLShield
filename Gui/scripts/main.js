3124bd1af58ac58d2a691be276556c4d9e05182a
tenways <rdasmith01@icloud.com>
2023-09-23 16:14:18 +0100

    refactor

--- Gui/scripts/main.js
+++ Gui/scripts/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 603,
+    width: 1080,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('../index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
